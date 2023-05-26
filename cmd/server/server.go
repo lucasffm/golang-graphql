6a358e25b1854e57d629458559e05fbde0313543
Lucas Mendes <lucasffm@gmail.com>
2023-05-26 16:30:12 -0300

    feat: initial commit

---
+++ cmd/server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/lucasffm/golang-graphql/graph"
+	"github.com/lucasffm/golang-graphql/internal/database"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./sqlite3.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Inject the database into database struct
+	categoryDB := database.NewCategory(db)
+	courseDB := database.NewCourse(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	// Create the resolver and passes models with the database
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+		CourseDB:   courseDB,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
